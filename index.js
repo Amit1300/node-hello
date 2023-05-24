3e1eff8384bb14defbd87d7d4ddd8689c8d0832b
Amit Kumar Nagar <nagar.amit1999@gmail.com>
2023-05-24 18:39:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Amit!\n'
+  const msg = 'Hello Amit, I know you are an Arsenal fan !\n'
   res.end(msg);
 });
 
